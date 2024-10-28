594ec7540f1cdf406b25715ba43ef83983fd99a0
therrasl <noraslrasl@gmail.com>
2024-10-28 22:36:11 +0300

    refactor

--- src/features/session/login/ui/loginForm.tsx
+++ src/features/session/login/ui/loginForm.tsx
@@ -1,12 +1,11 @@
 import { Button, Form, Input } from "antd";
 import s from "./loginForm.module.scss";
 
-
 export const LoginForm = () => {
   return (
     <Form className={s.form}>
       <h2>Please Login</h2>
-   
+
       <Input className={s.formInput} type="text" />
 
       <Input className={s.formInput} type="password" name="password" />
---
+++ src/shared/api/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { getTokenFromLocalStorage } from "../utils/localstorage";
+
+export const API_URL = axios.create({
+  baseURL: "https://localhost:4200/api",
+  headers: {
+    Authorization: `Bearer` + getTokenFromLocalStorage() ,
+  },
+});
